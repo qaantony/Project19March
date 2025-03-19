606d6b376f51d73ee313dc0bb142567d08a94fcb
qaantony <33860596+qaantony@users.noreply.github.com>
2025-03-19 09:23:07 +0530

    added new project
    added new project

---
+++ src/test/java/org/example1/AppTest.java
@@ -0,0 +1,18 @@
+
+package org.example1;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+    @Test
+    public void test1(){
+        System.out.println("##### Test1  ###################################>>>>>");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("##### Test2  ###################################>>>>>");
+        System.out.println("hello ########>>>>>>>");
+    }
+
+}
